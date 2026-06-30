1a1adf41223199570ac0e1719bb0b46c3d111687
vasylkartychak <w.i.k.mailua@gmail.com>
2026-06-30 20:49:01 +0300

    setup libs

---
+++ settings.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    URI: str
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra='ignore',
+    )
+
+
+settings = Settings()
