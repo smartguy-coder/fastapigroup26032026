f3fdfb7f51e5c8dd9d3fef536d6d9f99544cdafb
vasylkartychak <w.i.k.mailua@gmail.com>
2026-06-30 21:13:52 +0300

    save book

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     URI: str
+    SHOP_NAME_DB: str = 'shop'
+    BOOKS_COLLECTION: str = 'books'
 
     model_config = SettingsConfigDict(
         env_file='.env',
