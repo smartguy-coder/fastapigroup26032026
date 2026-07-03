fd162716c56fb76100a01db4421692e3cc3ed44b
vasylkartychak <w.i.k.mailua@gmail.com>
2026-07-03 21:16:26 +0300

    crud

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     URI: str
-    SHOP_NAME_DB: str = 'shop'
-    BOOKS_COLLECTION: str = 'books'
+    SHOP_NAME_DB: str = 'shop22'
+    BOOKS_COLLECTION: str = 'books222'
 
     model_config = SettingsConfigDict(
         env_file='.env',
